c18d4897e0259e403089e7912ede4e0ec4ec90ff
MeloAguilar <carmelo.aguilar@iesnervion.es>
2020-12-11 14:07:08 +0100

    Segundo commit donde volmevemos atrás

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //He cambiado Buenas Mundo por Carmelo
-        System.out.println("Carmelo");
+        System.out.println("Buenas Mundo");
     }
 }
